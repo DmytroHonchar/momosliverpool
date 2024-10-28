31e654b8bf87fc2315da2853717709a2b4f86e9e
DmytroHonchar <126788840+DmytroHonchar@users.noreply.github.com>
2024-10-28 14:39:37 +0000

    Responsive home page
    Responsive design for home page

--- src/script.js
+++ src/script.js
@@ -1,4 +1,14 @@
 function toggleNav() {
     const overlay = document.querySelector('.nav-overlay');
-    overlay.classList.toggle('active');
+    const body = document.querySelector('body');
+
+    if (!overlay.classList.contains('active')) {
+        overlay.classList.remove('inactive');
+        overlay.classList.add('active');
+        body.classList.add('no-scroll');
+    } else {
+        overlay.classList.remove('active');
+        overlay.classList.add('inactive');
+        body.classList.remove('no-scroll');
+    }
 }
